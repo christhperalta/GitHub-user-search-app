f70ddb8f907c6ab81e53d40cf0155031dcf4ed20
christh-peralta <christhperalta@gmail.com>
2023-07-04 12:37:10 -0400

    Updating Form margin-bottom

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from "../provider/ThemeContext";
 export default function Container({ children }) {
   const { theme } = useContext(ThemeContext);
   return (
-    <div className={`Container ${theme ? "theme-dark" : "theme-light"}`}>
+    <div className={`Container ${theme ? "theme-light" : "theme-dark"}`}>
       {children}
     </div>
   );
